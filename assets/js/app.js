168e69dbe00266f97ce9b52512afebb569359fe8
Akhil P A <akhilpa1085@gamil.com>
2023-04-19 11:01:43 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".bannerSwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
